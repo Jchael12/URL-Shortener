712d7e16d84e914b321518a082e81cc93f5d7c6b
Jchael12 <johnmichaelabbas23@gmail.com>
2023-05-30 21:47:53 +0800

    URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Url_Shortener/",
+  base: "/vite-deploy-demo/",
 });
